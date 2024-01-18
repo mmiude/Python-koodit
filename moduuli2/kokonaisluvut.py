ece438c8a360c758f79a55455758a4050d72bbdf
Mmiud <mmiude@outlook.com>
2024-01-18 17:37:06 +0200

    kokonaisluvut

---
+++ moduuli2/kokonaisluvut.py
@@ -0,0 +1,7 @@
+luku1 = int(input("Syötä luku: "))
+luku2 = int(input("Syötä luku: "))
+luku3 = int(input("Syötä luku: "))
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+print(f"Summa: {summa}" + f"\nTulo: {tulo}" + f"\nKeskiarvo: {keskiarvo}")
