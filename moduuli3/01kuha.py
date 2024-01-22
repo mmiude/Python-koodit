7d1add0ca45c85e127d23755f5100a11e67d6859
Mmiud <mmiude@outlook.com>
2024-01-22 16:51:16 +0200

    .

---
+++ moduuli3/01kuha.py
@@ -0,0 +1,4 @@
+kuhan_pituus = float(input("Anna kuhan pituus: "))
+if kuhan_pituus < 37:
+    print(f"Laske takaisin järveen. Pituudesta puuttuu: {37 - kuhan_pituus}cm")
+else: print("Pidä kuha!")
---
+++ moduuli3/02hyttiluokka.py
@@ -0,0 +1,11 @@
+hytti_luokka = input("Anna hyttiluokka: ")
+if hytti_luokka == "LUX":
+    print("Parvekkeellinen hytti yläkannella.")
+elif hytti_luokka == "A":
+    print("Ikkunallinen hytti autokannen yläpuolella.")
+elif hytti_luokka == "B":
+    print("Ikkunaton hytti autokannen yläpuolella.")
+elif hytti_luokka == "C":
+    print("Ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen hyttiluokka.")
